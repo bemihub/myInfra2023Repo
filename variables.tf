f1c5a10959ef03cf19d4774a8c6ea10e6da4e578
bemihub <68174373+bemihub@users.noreply.github.com>
2024-04-09 12:55:36 -0500

    Update variables.tf
    Fixed AWS Ubuntu Key

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myKey101722" 
+    default     =  "UbuntuKey" 
 }
 
 variable "instance_type" { 
